cfb4b25276713de3fced21e45c23ac21c480b132
macbookshop <naly.moslih48@gmail.com>
2023-02-11 04:41:48 +0300

    update💣

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-// Get all the modal elements
 var modals = document.querySelectorAll(".modal");
 
-// Get the portfolio items
 var portfolioItems = document.querySelectorAll(".portfolio-list a");
 
-// Loop through the portfolio items and add click event listeners
 for (var i = 0; i < portfolioItems.length; i++) {
   portfolioItems[i].addEventListener("click", function (event) {
     event.preventDefault();
 
-    // Get the modal that corresponds to the clicked portfolio item
     var modalId = this.getAttribute("data-modal");
     var modal = document.querySelector("#" + modalId);
 
-    // Show the modal
     modal.style.display = "block";
   });
 }
 
-// Loop through the modals and add click event listeners to close button
 for (var i = 0; i < modals.length; i++) {
   var closeButton = modals[i].querySelector(".close");
   closeButton.addEventListener("click", function () {
@@ -26,7 +20,6 @@ for (var i = 0; i < modals.length; i++) {
   });
 }
 
-// Add event listener to the window to close modals when clicking outside the modal content
 window.addEventListener("click", function (event) {
   if (event.target.className === "modal") {
     event.target.style.display = "none";
